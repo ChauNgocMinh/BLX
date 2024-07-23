e5421962d4372b1398a2b294ea786bd55e360933
ChauNgocMinh <chaungocminh202@gmail.com>
2024-07-23 11:12:46 +0700

    update

--- BLX/wwwroot/js/interop.js
+++ BLX/wwwroot/js/interop.js
@@ -27,3 +27,11 @@ function base64ToBlob(base64, contentType) {
     const blob = new Blob(byteArrays, { type: contentType });
     return blob;
 }
+function downloadFile(fileName, base64) {
+    const link = document.createElement('a');
+    link.href = 'data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,' + base64;
+    link.download = fileName;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
